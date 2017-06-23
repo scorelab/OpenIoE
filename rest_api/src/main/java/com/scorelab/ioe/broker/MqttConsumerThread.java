f7af31437eda106e27cd6491624abf38e5718275
username <priyapappachan010@gmail.com>
2017-06-23 12:08:26 +0530

    topic subscribe using mqtt

--- rest_api/src/main/java/com/scorelab/ioe/broker/MqttConsumerThread.java
+++ rest_api/src/main/java/com/scorelab/ioe/broker/MqttConsumerThread.java
@@ -2,9 +2,16 @@ package com.scorelab.ioe.broker;
 
 import com.scorelab.ioe.config.Constants;
 import com.scorelab.ioe.config.IoeConfiguration;
+import com.scorelab.ioe.repository.SubscriptionRepository;
+import com.scorelab.ioe.domain.Subscription;
 import org.fusesource.mqtt.client.BlockingConnection;
 import org.fusesource.mqtt.client.MQTT;
+import org.fusesource.mqtt.client.QoS;
+import org.fusesource.mqtt.client.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.ArrayList;
 
 import java.net.URISyntaxException;
 
@@ -15,6 +22,7 @@ public class MqttConsumerThread {
 
     @Autowired
     private IoeConfiguration ioeConfiguration;
+    private SubscriptionRepository subscriptionRepository;
 
     public void StartBrokerConsumer() throws Exception {
         MQTT mqtt = new MQTT();
@@ -22,5 +30,12 @@ public class MqttConsumerThread {
         BlockingConnection connection = mqtt.blockingConnection();
         connection.connect();
 
+        List<Topic> topic_list = new ArrayList<>();
+        List<Subscription> subscriptions = subscriptionRepository.findAll();
+        for (Subscription s : subscriptions) {
+            topic_list.add(new Topic(s.getTopicFilter(), QoS.AT_LEAST_ONCE));
+        }
+        Topic[] topics = topic_list.toArray(new Topic[topic_list.size()]);
+        connection.subscribe(topics);
     }
 }
