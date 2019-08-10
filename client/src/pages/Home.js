41d3ada640100b7ab4a0d84958986eca5096a81f
dande9304 <dande9304@gmail.com>
2019-08-10 13:12:20 +1000

    added footer

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import EpicMenu from '../EpicMenu';
+
+class Home extends Comment {
+    render() {
+        <EpicMenu />
+        <ListItems />
+        <FooterPage />
+    }
+}
